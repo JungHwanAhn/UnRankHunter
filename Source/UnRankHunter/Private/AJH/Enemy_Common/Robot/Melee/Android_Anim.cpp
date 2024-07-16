8ebe7dcbde5bcfb6026849ad22c08a4d6cf09d85
JungHwanAhn <wjdghks1831@naver.com>
2024-07-16 11:22:27 +0900

    Feat: Android (공격, 죽음)애니메이션 추가
    Android 몬스터 공격, 죽음 애니메이션 추가

--- Source/UnRankHunter/Private/AJH/Enemy_Common/Robot/Melee/Android_Anim.cpp
+++ Source/UnRankHunter/Private/AJH/Enemy_Common/Robot/Melee/Android_Anim.cpp
@@ -4,12 +4,28 @@
 
 UAndroid_Anim::UAndroid_Anim()
 {
-	
+	static ConstructorHelpers::FObjectFinder<UAnimMontage>
+		Attack_Montage(TEXT("AnimMontage'/Game/01_Core/AJH/Enemy/Robot/Melee/Montage/AJH_AM_Android_Attack.AJH_AM_Android_Attack'"));
+	if (Attack_Montage.Succeeded()) {
+		AttackMontage = Attack_Montage.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UAnimMontage>
+		Die_Montage(TEXT("AnimMontage'/Game/01_Core/AJH/Enemy/Robot/Melee/Montage/AJH_AM_Android_Die.AJH_AM_Android_Die'"));
+	if (Die_Montage.Succeeded()) {
+		DieMontage = Die_Montage.Object;
+	}
 }
 
 void UAndroid_Anim::Attack(FString pattern)
 {
+	Montage_Play(AttackMontage);
+	Montage_JumpToSection(*pattern);
+}
 
+void UAndroid_Anim::Die()
+{
+	Montage_Play(DieMontage);
 }
 
 void UAndroid_Anim::NativeInitializeAnimation()
--- Source/UnRankHunter/Public/AJH/Enemy_Common/Robot/Melee/Android_Anim.h
+++ Source/UnRankHunter/Public/AJH/Enemy_Common/Robot/Melee/Android_Anim.h
@@ -12,6 +12,7 @@ class UNRANKHUNTER_API UAndroid_Anim : public UAnimInstance
 public:
 	UAndroid_Anim();
 	void Attack(FString pattern);
+	void Die();
 
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Android State")
@@ -23,6 +24,12 @@ protected:
 private:
 	virtual void NativeInitializeAnimation() override;
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Montage")
+	UAnimMontage* AttackMontage;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Montage")
+	UAnimMontage* DieMontage;
 
 	class APawn* Owner;
 	class AAndroid* Android;
