49117075660a2f0164d3658227b724938d745b7f
JungHwanAhn <wjdghks1831@naver.com>
2024-07-16 12:36:11 +0900

    Comment: 주석 추가
    헤더파일 내 주석 추가

--- Source/UnRankHunter/Public/AJH/Subsystem/PoolSubsystem.h
+++ Source/UnRankHunter/Public/AJH/Subsystem/PoolSubsystem.h
@@ -4,6 +4,7 @@
 #include "Subsystems/WorldSubsystem.h"
 #include "PoolSubsystem.generated.h"
 
+// Structure to hold an array of pooled actors
 USTRUCT()
 struct FPoolArray {
 	GENERATED_BODY()
@@ -11,14 +12,17 @@ struct FPoolArray {
 	UPROPERTY()
 	TArray<AActor*> ObjectPool;
 
+	// Check if the pool is empty
 	bool IsEmpty() {
 		return ObjectPool.IsEmpty();
 	}
 
+	// Pop an actor from the pool (removes and returns the last actor)
 	AActor* Pop() {
 		return ObjectPool.Pop();
 	}
 
+	// Add an actor to the pool
 	void Add(AActor* ActorToAdd) {
 		ObjectPool.Add(ActorToAdd);
 	} 
@@ -30,17 +34,22 @@ class UNRANKHUNTER_API UPoolSubsystem : public UWorldSubsystem
 	GENERATED_BODY()
 	
 public:
+	// Function to spawn an actor from the pool
 	UFUNCTION(BlueprintCallable, Category = "Pool SubSystem")
 	void SpawnFromPool(TSubclassOf<AActor> PoolClass, FVector Location, FRotator Rotation, AActor*& SpawnedActor);
 
+	// Function to return an actor to the pool
 	UFUNCTION(BlueprintCallable, Category = "Pool SubSystem")
 	void ReturnToPool(AActor* Poolable);
 
+	// Function to initialize the pool for a specific class with a maximum size
 	UFUNCTION(BlueprintCallable, Category = "Pool SubSystem")
 	void InitializePool(TSubclassOf<AActor> PoolClass, int32 MaxSize);
 
 private:
+	// Map of object pools, keyed by class
 	TMap<UClass*, FPoolArray> ObjectPools;
 
+	// Helper function to get an actor from the pool or create a new one if necessary
 	AActor* GetActorFromPool(TSubclassOf<AActor> PoolClass, FVector Location, FRotator Rotation);
 };
