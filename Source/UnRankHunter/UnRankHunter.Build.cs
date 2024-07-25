a96b5ab5447b518a126a36ed6a87f7a823d4d2e2
JungHwanAhn <wjdghks1831@naver.com>
2024-07-25 16:50:41 +0900

    Feat: Skeleton Enemy 개발에 따른 폴더 경로 지정
    Skeleton Enemy 개발에 따른 폴더 경로 지정

--- Source/UnRankHunter/UnRankHunter.Build.cs
+++ Source/UnRankHunter/UnRankHunter.Build.cs
@@ -12,8 +12,9 @@ public class UnRankHunter : ModuleRules
 
 		// AJH
 		PublicIncludePaths.AddRange(new string[] {
-			"UnRankHunter/Public/AJH/Interface", "UnRankHunter/Public/AJH/Subsystem", "UnRankHunter/Public/AJH/Enemy_Common",
-			"UnRankHunter/Public/AJH/Enemy_Common/Robot", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Melee", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Range", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Elite" });
+			"UnRankHunter/Public/AJH/Interface", "UnRankHunter/Public/AJH/Subsystem", "UnRankHunter/Public/AJH/Enemy_Common", "UnRankHunter/Public/AJH/Enemy_Common/AIController",
+            "UnRankHunter/Public/AJH/Enemy_Common/Robot", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Melee", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Range", "UnRankHunter/Public/AJH/Enemy_Common/Robot/Elite",
+            "UnRankHunter/Public/AJH/Enemy_Common/Skeleton", "UnRankHunter/Public/AJH/Enemy_Common/Skeleton/Melee", "UnRankHunter/Public/AJH/Enemy_Common/Skeleton/Range", "UnRankHunter/Public/AJH/Enemy_Common/Skeleton/Elite" });
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AnimGraphRuntime", "AIModule" });
 
