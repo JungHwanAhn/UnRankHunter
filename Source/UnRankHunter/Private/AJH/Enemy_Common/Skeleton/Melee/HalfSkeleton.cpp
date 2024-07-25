98b44bbe2ce779028cb92974aa57da83233438d8
JungHwanAhn <wjdghks1831@naver.com>
2024-07-25 16:48:26 +0900

    Feat: Half Skeleton Enemy 개발
    Half Skeleton Enemy 개발
    - 이동, 공격, 피격, 죽음 기능 및 애니메이션 개발
    - Skeletal, Weapon 미지정으로 자유롭게 사용 가능

---
+++ Source/UnRankHunter/Private/AJH/Enemy_Common/Skeleton/Melee/HalfSkeleton.cpp
@@ -0,0 +1,109 @@
+#include "HalfSkeleton.h"
+#include "HalfSkeleton_Anim.h"
+#include "AIController_Common.h"
+#include "Components/BoxComponent.h"
+#include "Components/CapsuleComponent.h"
+
+AHalfSkeleton::AHalfSkeleton()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	AIControllerClass = AAIController_Common::StaticClass();
+
+	GetMesh()->SetRelativeLocation(FVector(0, 0, -100));
+	GetMesh()->SetRelativeScale3D(FVector(1.1));
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SkeletonWeapon = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SkeletonWeapon"));
+	SkeletonWeapon->SetupAttachment(GetMesh());
+	SkeletonWeapon->AttachToComponent(GetMesh(), FAttachmentTransformRules::KeepRelativeTransform, TEXT("RightHand"));
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance>
+		AnimInstance(TEXT("/Game/01_Core/AJH/Enemy/Skeleton/Melee/AJH_ABP_HalfSkeleton.AJH_ABP_HalfSkeleton_C"));
+	if (AnimInstance.Succeeded()) {
+		GetMesh()->SetAnimInstanceClass(AnimInstance.Class);
+	}
+
+	RHCollision->SetBoxExtent(FVector(20, 20, 80));
+	RHCollision->SetRelativeLocation(FVector(0, 0, 75));
+
+	GetCapsuleComponent()->InitCapsuleSize(20.0f, 90.0f);
+}
+
+void AHalfSkeleton::Attack()
+{
+	if (!bIsEnemyDie) {
+		Super::Attack();
+
+		damage = 20.0f;
+		randomPattern = FMath::RandRange(1, 9);
+
+		UHalfSkeleton_Anim* HalfSkeletonAnim = Cast<UHalfSkeleton_Anim>(GetMesh()->GetAnimInstance());
+		if (HalfSkeletonAnim == nullptr) return;
+
+		if (randomPattern <= 3) {
+			HalfSkeletonAnim->Attack("Attack_A");
+		}
+		else if (randomPattern <= 6) {
+			HalfSkeletonAnim->Attack("Attack_B");
+		}
+		else {
+			HalfSkeletonAnim->Attack("Attack_C");
+		}
+
+		HalfSkeletonAnim->OnMontageEnded.RemoveDynamic(this, &AHalfSkeleton::OnAttackMontageEnded);
+		HalfSkeletonAnim->OnMontageEnded.AddDynamic(this, &AHalfSkeleton::OnAttackMontageEnded);
+	}
+}
+
+void AHalfSkeleton::OnAttackMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	Super::OnAttackMontageEnded(Montage, bInterrupted);
+}
+
+void AHalfSkeleton::AttackCheckOverlap(UPrimitiveComponent* OverlapComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::AttackCheckOverlap(OverlapComp, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+float AHalfSkeleton::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	float actualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+	if (actualDamage > 0.f && !bIsEnemyDie) {
+		halfSkeletonHP -= actualDamage;
+		if (halfSkeletonHP <= 0.f) EnemyDie();
+	}
+	return actualDamage;
+}
+
+void AHalfSkeleton::EnemyDie()
+{
+	bIsEnemyDie = true;
+
+	UHalfSkeleton_Anim* HalfSkeletonAnim = Cast<UHalfSkeleton_Anim>(GetMesh()->GetAnimInstance());
+	if (HalfSkeletonAnim == nullptr) return;
+
+	HalfSkeletonAnim->Die();
+
+	FTimerHandle DieTimerHandle;
+	FTimerDelegate CallEnemyDie = FTimerDelegate::CreateLambda([this]() { Super::EnemyDie(); });
+	GetWorld()->GetTimerManager().SetTimer(DieTimerHandle, CallEnemyDie, dieDelay, false);
+}
+
+void AHalfSkeleton::OnSpawnFromPool_Implementation()
+{
+	Super::OnSpawnFromPool_Implementation();
+}
+
+void AHalfSkeleton::OnReturnToPool_Implementation()
+{
+	Super::OnReturnToPool_Implementation();
+}
+
+void AHalfSkeleton::OnCollisionStart_Implementation()
+{
+}
+
+void AHalfSkeleton::OnCollisionEnd_Implementation()
+{
+}
