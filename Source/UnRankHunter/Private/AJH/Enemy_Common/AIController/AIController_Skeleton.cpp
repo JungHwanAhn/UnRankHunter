67591f398ca092df13309d42de9b238f7a5351da
JungHwanAhn <wjdghks1831@naver.com>
2024-07-26 15:52:05 +0900

    Feat: Skeleton Enemy AIController 개발
    Skeleton Enemy AIController 개발

---
+++ Source/UnRankHunter/Private/AJH/Enemy_Common/AIController/AIController_Skeleton.cpp
@@ -0,0 +1,82 @@
+#include "AIController_Skeleton.h"
+#include "BaseEnemy_Common.h"
+#include "Kismet/GameplayStatics.h"
+
+void AAIController_Skeleton::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    if (PlayerPawn) {
+        if (ControlledPawn->bIsActive && !ControlledPawn->bIsEnemyDie) {
+            SetFocus(PlayerPawn);
+            lastEnemyScan += DeltaSeconds;
+
+            if (!bIsAttack) {
+                FVector Destination = PlayerPawn->GetActorLocation();
+                FVector CurrentLocation = ControlledPawn->GetActorLocation();
+
+                if (lastEnemyScan >= enemyScanInterval)
+                {
+                    EnemyScan();
+                    lastEnemyScan = 0.0f;
+                }
+
+                FVector AdjustedDestination = Destination + AvoidanceVector;
+
+                MoveToLocation(AdjustedDestination, acceptanceRadius);
+
+                float distance = FVector::Distance(this->GetPawn()->GetActorLocation(), PlayerPawn->GetActorLocation());
+                if (distance < 250.0f)
+                {
+                    bIsAttack = true;
+                    FTimerHandle AttackTimerHandle;
+                    FTimerDelegate AttackCD = FTimerDelegate::CreateLambda([this]() { bIsAttack = false; });
+                    GetWorld()->GetTimerManager().SetTimer(AttackTimerHandle, AttackCD, attackDelay, false);
+                    ControlledPawn->Attack();
+                }
+            }
+        }
+        else {
+            ClearFocus(EAIFocusPriority::Gameplay);
+        }
+    }
+    else {
+        ClearFocus(EAIFocusPriority::Gameplay);
+    }
+}
+
+void AAIController_Skeleton::OnPossess(APawn* InPawn)
+{
+    Super::OnPossess(InPawn);
+
+    if (InPawn) {
+        PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+        ControlledPawn = Cast<ABaseEnemy_Common>(InPawn);
+    }
+}
+
+void AAIController_Skeleton::EnemyScan()
+{
+    if (ControlledPawn)
+    {
+        FVector CurrentLocation = ControlledPawn->GetActorLocation();
+
+        TArray<AActor*> OverlappingEnemies;
+        UKismetSystemLibrary::SphereOverlapActors(GetWorld(), CurrentLocation, avoidanceRadius, TArray<TEnumAsByte<EObjectTypeQuery>>(), ABaseEnemy_Common::StaticClass(), TArray<AActor*>(), OverlappingEnemies);
+
+        AvoidanceVector = FVector::ZeroVector;
+        for (AActor* Actor : OverlappingEnemies)
+        {
+            if (Actor != ControlledPawn)
+            {
+                FVector ToOther = CurrentLocation - Actor->GetActorLocation();
+                float DistanceToOther = ToOther.Size();
+
+                if (DistanceToOther < avoidanceRadius)
+                {
+                    AvoidanceVector += ToOther.GetSafeNormal() * (avoidanceRadius - DistanceToOther) * avoidanceStrength;
+                }
+            }
+        }
+    }
+}
