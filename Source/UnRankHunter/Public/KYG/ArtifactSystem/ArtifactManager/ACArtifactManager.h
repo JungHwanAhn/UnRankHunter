b8315336b3817730692ca0f8625feb8415e62fab
ianm0322 <46345017+ianm0322@users.noreply.github.com>
2024-08-14 11:11:13 +0900

    Update: 사소한 변경사항
    개발 환경에 영향을 끼치지 않는 미비한 변경사항 커밋.

--- Source/UnRankHunter/Public/KYG/ArtifactSystem/ArtifactManager/ACArtifactManager.h
+++ Source/UnRankHunter/Public/KYG/ArtifactSystem/ArtifactManager/ACArtifactManager.h
@@ -13,16 +13,13 @@ class UNRANKHUNTER_API UACArtifactManager : public UActorComponent
 	GENERATED_BODY()
 
 public:	
-	// Sets default values for this component's properties
 	UACArtifactManager();
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	
 };
